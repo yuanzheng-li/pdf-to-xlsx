3a932417745e72b6d96e6c31c670cdf541384529
Yuanzheng Li <yuanzhengliwork@gmail.com>
2020-08-03 22:30:43 -0700

    refactor and document differences between two formats

--- src/parser.js
+++ src/parser.js
@@ -43,150 +43,190 @@ const contentPattern = {
 function extractData(pages) {
   const data = [];
   pages.forEach((page) => {
-    const pageNo = page['1.00'][1].text;
+    const pageHeader = extractHeader(page);
 
-    let county = '';
-
-    if (!_.isUndefined(page[headerPattern.county.row][1])) {
-      county += page[headerPattern.county.row][1].text;
-    } 
-    if (page[headerPattern.county.altRow] && !_.isUndefined(page[headerPattern.county.altRow][0])) {
-      county +=
-        county.length === 0
-          ? `${page[headerPattern.county.altRow][0].text}`
-          : ` ${page[headerPattern.county.altRow][0].text}`;
-    }
-
-    console.assert(
-      county.length !== 0,
-      `county not found on page ${pageNo}`
-    );
-
-    const reportDate = page[headerPattern.reportDate.row][1].text;
-    const year = parseInt(reportDate.match(headerPattern.reportDate.pattern)[0]);
-    console.assert(
-      !_.isUndefined(year) && !_.isUndefined(reportDate),
-      `report year not found on page ${pageNo}`
-    );
-
-    const keys = Object.keys(page).sort((a, b) => parseFloat(a) - parseFloat(b)).filter((key) => parseFloat(key) > 7);
-
-    keys.forEach((key, index) => {
-      // curContent could have duplicate items
-      // year: 2019; ID:73000237 has GS110SPid: 19000347
-      // year: 2019; id: 3655257
-      // year: 2019; id: 99000085
-      const curContent = _.uniqBy(page[key], 'x');
-      console.assert(!_.isUndefined(curContent), `content not found on page ${pageNo}`);
-      const item = {};
-
-      if(contentPattern.type.test(curContent[0].text)) {
-        const matched = curContent[0].text.match(contentPattern.type);
-        let qris = '';
-        let type = '';
-        if(!_.isUndefined(matched[1]) && !_.isUndefined(matched[2])) {
-          qris = parseInt(matched[1]);
-          type = matched[2];
-        } else {
-          type = matched[0];
-        }
-
-        const prevContent = page[keys[index - 1]];
-        console.assert(!_.isUndefined(prevContent), `Did not find the main row with data on page ${pageNo} and row ${key}`);
-
-        for(let i = 1; i < curContent.length; i++) {
-          const prevItem = _.find(prevContent, { x: curContent[i].x });
-          console.assert(
-            !_.isUndefined(prevItem),
-            `Did not find the multi-line data element on page ${pageNo} with id ${prevContent[0].text}`
-          );
-          const prevText = prevItem.text;
-          const curText = curContent[i].text;
-
-          prevItem.text = `${prevText} ${curText}`;
-        }
-
-        // TODO: pay attention when parsing data from other years
-        if(contentPattern.id.test(prevContent[0].text)) {
-          if(prevContent.length !== 18) {
-            console.warn(`Outlier: length is ${prevContent.length}. ID is ${prevContent[0].text}. Year: ${year}`);
-          }
-          let usefulContent = [];
-          if(prevContent.length === 16) {
-            // length == 16 no category operation and operation site;
-            usefulContent = [...prevContent.slice(0, 3), ...prevContent.slice(4, 15), '', '', prevContent[15]];
-          } else if(prevContent.length === 17) {
-            // 17 no category operation or operation site;
-            // TODO: create category operation list and operation site list to handle length == 17??
-            // 37.58 38.23 38.09 37.16 36.78 37.58 37.25 38.19 36.83 37.30 37.30 42.13 42.92
-            // if x < 40, the item on index 15 is category operation; x > 40 operation site
-            if(parseFloat(prevContent[15].x) > 40) {
-              usefulContent = [
-                ...prevContent.slice(0, 3),
-                ...prevContent.slice(4, 15),
-                '',
-                ...prevContent.slice(15),
-              ];
-            } else {
-              usefulContent = [
-                ...prevContent.slice(0, 3),
-                ...prevContent.slice(4, 16),
-                '',
-                prevContent[16],
-              ];
-            }
-          } else if(prevContent.length === 18) {
-            // 18 normal;
-            usefulContent = [...prevContent.slice(0, 3), ...prevContent.slice(4)];
-          } else if(prevContent.length === 19) {
-            // 19 index at 1 and index at 2 should be merged as the operation name;
-            usefulContent = [
-              prevContent[0],
-              {
-                text: prevContent[1].text + prevContent[2].text,
-                x: prevContent[1].x,
-              },
-              prevContent[3],
-              ...prevContent.slice(5)
-            ];
-          } else if(prevContent.length === 20) {
-            // 20 index at 1, 2, 3 should be merged as the operation name;
-            usefulContent = [
-              prevContent[0],
-              {
-                text: prevContent[1].text + prevContent[2].text + prevContent[3].text,
-                x: prevContent[1].x,
-              },
-              prevContent[4],
-              ...prevContent.slice(6),
-            ];
-          } else if(prevContent.length === 36) {
-            // 36 only need the 0 - 17
-            usefulContent = [
-              ...prevContent.slice(0, 3),
-              ...prevContent.slice(4, 18),
-            ];
-          }
-
-          usefulContent.forEach((value, index) => {
-            value = value.text;
-            if(columns[index].int) {
-              value = parseInt(value);
-            }
-            item[columns[index].key] = value;
-          });
-          
-          item.county = county;
-          item.year = year;
-          item.qris = qris;
-          item.type = type;
-        }
-
-        data.push(item);
-      }
-    });
+    extractContent(data, page, pageHeader);
   });
   return data;
+}
+
+/**
+ * before 2016, 5-STAR
+ * before 2016, operation name can take up 3 lines
+ * before 2016, need another way to differentiate detail page and summary page, maybe the table header?
+ */
+function extractContent(data, page, pageHeader) {
+  const keys = Object.keys(page)
+    .sort((a, b) => parseFloat(a) - parseFloat(b))
+    .filter((key) => parseFloat(key) > 7);
+
+  keys.forEach((key, index) => {
+    // curContent could have duplicate items
+    // year: 2019; ID:73000237 has GS110SPid: 19000347
+    // year: 2019; id: 3655257
+    // year: 2019; id: 99000085
+    const curContent = _.uniqBy(page[key], 'x');
+    console.assert(
+      !_.isUndefined(curContent),
+      `content not found on page ${pageHeader.pageNo}`
+    );
+    const item = {};
+
+    if (contentPattern.type.test(curContent[0].text)) {
+      const matched = curContent[0].text.match(contentPattern.type);
+      let qris = '';
+      let type = '';
+      if (!_.isUndefined(matched[1]) && !_.isUndefined(matched[2])) {
+        qris = parseInt(matched[1]);
+        type = matched[2];
+      } else {
+        type = matched[0];
+      }
+
+      const prevContent = page[keys[index - 1]];
+      console.assert(
+        !_.isUndefined(prevContent),
+        `Did not find the main row with data on page ${pageHeader.pageNo} and row ${key}`
+      );
+
+      for (let i = 1; i < curContent.length; i++) {
+        const prevItem = _.find(prevContent, { x: curContent[i].x });
+        console.assert(
+          !_.isUndefined(prevItem),
+          `Did not find the multi-line data element on page ${pageHeader.pageNo} with id ${prevContent[0].text}`
+        );
+        const prevText = prevItem.text;
+        const curText = curContent[i].text;
+
+        prevItem.text = `${prevText} ${curText}`;
+      }
+
+      // TODO: pay attention when parsing data from other years
+      if (contentPattern.id.test(prevContent[0].text)) {
+        if (prevContent.length !== 18) {
+          console.warn(
+            `Outlier: length is ${prevContent.length}. ID is ${prevContent[0].text}. Year: ${pageHeader.year}`
+          );
+        }
+        let usefulContent = [];
+        if (prevContent.length === 16) {
+          // length == 16 no category operation and operation site;
+          usefulContent = [
+            ...prevContent.slice(0, 3),
+            ...prevContent.slice(4, 15),
+            '',
+            '',
+            prevContent[15],
+          ];
+        } else if (prevContent.length === 17) {
+          // 17 no category operation or operation site;
+          // TODO: create category operation list and operation site list to handle length == 17??
+          // 37.58 38.23 38.09 37.16 36.78 37.58 37.25 38.19 36.83 37.30 37.30 42.13 42.92
+          // if x < 40, the item on index 15 is category operation; x > 40 operation site
+          if (parseFloat(prevContent[15].x) > 40) {
+            usefulContent = [
+              ...prevContent.slice(0, 3),
+              ...prevContent.slice(4, 15),
+              '',
+              ...prevContent.slice(15),
+            ];
+          } else {
+            usefulContent = [
+              ...prevContent.slice(0, 3),
+              ...prevContent.slice(4, 16),
+              '',
+              prevContent[16],
+            ];
+          }
+        } else if (prevContent.length === 18) {
+          // 18 normal;
+          usefulContent = [...prevContent.slice(0, 3), ...prevContent.slice(4)];
+        } else if (prevContent.length === 19) {
+          // 19 index at 1 and index at 2 should be merged as the operation name;
+          usefulContent = [
+            prevContent[0],
+            {
+              text: prevContent[1].text + prevContent[2].text,
+              x: prevContent[1].x,
+            },
+            prevContent[3],
+            ...prevContent.slice(5),
+          ];
+        } else if (prevContent.length === 20) {
+          // 20 index at 1, 2, 3 should be merged as the operation name;
+          usefulContent = [
+            prevContent[0],
+            {
+              text:
+                prevContent[1].text + prevContent[2].text + prevContent[3].text,
+              x: prevContent[1].x,
+            },
+            prevContent[4],
+            ...prevContent.slice(6),
+          ];
+        } else if (prevContent.length === 36) {
+          // 36 only need the 0 - 17
+          usefulContent = [
+            ...prevContent.slice(0, 3),
+            ...prevContent.slice(4, 18),
+          ];
+        }
+
+        usefulContent.forEach((value, index) => {
+          value = value.text;
+          if (columns[index].int) {
+            value = parseInt(value);
+          }
+          item[columns[index].key] = value;
+        });
+
+        item.county = pageHeader.county;
+        item.year = pageHeader.year;
+        item.qris = qris;
+        item.type = type;
+      }
+
+      data.push(item);
+    }
+  });
+}
+
+/**
+ * before 2016, the county only takes up 1 line
+ * 
+ */
+function extractHeader(page) {
+  const pageNo = page['1.00'][1].text;
+
+  let county = '';
+
+  if (!_.isUndefined(page[headerPattern.county.row][1])) {
+    county += page[headerPattern.county.row][1].text;
+  }
+  if (
+    page[headerPattern.county.altRow] &&
+    !_.isUndefined(page[headerPattern.county.altRow][0])
+  ) {
+    county +=
+      county.length === 0
+        ? `${page[headerPattern.county.altRow][0].text}`
+        : ` ${page[headerPattern.county.altRow][0].text}`;
+  }
+
+  console.assert(county.length !== 0, `county not found on page ${pageNo}`);
+
+  const reportDate = page[headerPattern.reportDate.row][1].text;
+  const year = parseInt(reportDate.match(headerPattern.reportDate.pattern)[0]);
+  console.assert(
+    !_.isUndefined(year) && !_.isUndefined(reportDate),
+    `report year not found on page ${pageNo}`
+  );
+
+  return {
+    year,
+    county,
+    pageNo
+  };
 }
 
 function parseData(pages) {
